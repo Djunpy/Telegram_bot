215bbe4307315e434b642881b3629fc26d6a3e01
Djunpy <djunpy@gmail.com>
2022-01-15 03:53:36 +0300

    init2

--- main.py
+++ main.py
@@ -5,45 +5,50 @@ from datetime import datetime
 import time
 
 
-def get_first_news():#
+def get_first_news():
+    """Получаем все новости"""
     headers = {
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
     }
-
-    url = "https://www.securitylab.ru/news/" #Ссылка на ресурс
-    r = requests.get(url=url, headers=headers)# url адрес запроса, и параметры закпроса
-
-    soup = BeautifulSoup(r.text, "lxml")#r.text полученная страница, lxml парсер
-    articles_cards = soup.find_all("a", class_="article-card")#Указываем контейнер с которого будем собирать информацию
-
-    news_dict = {}#Словарь для статей, он заполняется на каждой итерации цикла где id это ключ
-    for article in articles_cards: #В теле цикла указаны эллементы страницы которые нужно спарсить, циклом пробегаемся по этим элем и собираем инфу
+    # Ссылка на ресурс
+    url = "https://www.securitylab.ru/news/"
+    # url адрес запроса, и параметры закпроса
+    r = requests.get(url=url, headers=headers)
+    # r.text полученная страница, lxml парсер
+    soup = BeautifulSoup(r.text, "lxml")
+    # Указываем контейнер с которого будем собирать информацию
+    articles_cards = soup.find_all("a", class_="article-card")
+    # Словарь для статей, он заполняется на каждой итерации цикла где id это ключ
+    news_dict = {}
+    # В теле цикла указаны эллементы страницы которые нужно спарсить, циклом пробегаемся по этим элем и собираем инфу
+    for article in articles_cards:
         article_title = article.find("h2", class_="article-card-title").text.strip()
         article_desc = article.find("p").text.strip()
-        article_url = f'https://www.securitylab.ru{article.get("href")}'#Получаем ссылку статьи
+        # Получаем ссылку статьи
+        article_url = f'https://www.securitylab.ru{article.get("href")}'
 
         article_date_time = article.find("time").get("datetime")
         date_from_iso = datetime.fromisoformat(article_date_time)
         date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
+        # Преобразуем Unix timestamp
         article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
-        #Преобразуем Unix timestamp
 
-
-        #print(f"{article_title} | {article_url} | {article_date_timestamp}")
-        article_id = article_url.split("/")[-1]#Получаем id новости. Берется он из ссылки на статью и вычитаем 4 последних символа
-        article_id = article_id[:-4]#Все еще id новости
-
+        # Получаем id новости. Берется он из ссылки на статью и вычитаем 4 последних символа
+        article_id = article_url.split("/")[-1]
+        article_id = article_id[:-4]
+        # Инструкция сохранения контента id ключ получ данные это значения
         news_dict[article_id] = {
             "article_date_timestamp": article_date_timestamp,
             "article_title": article_title,
             "article_url": article_url,
             "article_desc": article_desc
-        }#Инструкция сохранения контента id ключ получ данные это значения
-
-        with open("news_dict.json", "w",) as file:#Сохраняем результат работы в json файл
+        }
+        # Сохраняем результат работы в json файл
+        with open("news_dict.json", "w",) as file:
             json.dump(news_dict, file, indent=4, ensure_ascii=False)
 
 def check_news_update():
+    """Проверяем появились ли свежие новости"""
     with open("news_dict.json", encoding='utf-8') as file:
         news_dict = json.load(file)
 
--- tg_bot.py
+++ tg_bot.py
@@ -10,20 +10,19 @@ from aiogram.utils.markdown import hbold, hlink
 from config import BOT_TOKEN, USER_ID
 
 
-
-
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands='start')
 async def get_fresh_news(message: types.Message):
+    """Создаем кнопки"""
+
     start_buttons = ['Все новости', 'Последние 5 новостей', 'На повестке']
     keyword = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyword.add(*start_buttons)
 
     await message.answer('Лента новостей', reply_markup=keyword)
-
 
 
 @dp.message_handler(Text(equals='Все новости'))
@@ -49,11 +48,14 @@ async def get_last_five_news(message: types.Message):
     for k, v in sorted(news_dict.items())[-5:]:
         news = f'{hbold(datetime.datetime.fromtimestamp(v["article_date_timestamp"]))}\n' \
                f'{hlink(v["article_title"], v["article_url"])}'
+
         await message.answer(news)
 
 
 @dp.message_handler(Text(equals='На повестке'))
 async def get_fresh_news(message: types.Message):
+    """Получаем свежие новости"""
+
     fresh_news = check_news_update()
 
     if fresh_news is not None:
@@ -62,12 +64,13 @@ async def get_fresh_news(message: types.Message):
                    f"{hlink(v['article_title'], v['article_url'])}"
 
             await message.answer(news)
-
     else:
+
         await message.answer("Пока нет свежих новостей...")
 
 
 async def news_every_minute():
+    """Получаем свежие новости каждые n-минут"""
     while True:
         fresh_news = check_news_update()
 
